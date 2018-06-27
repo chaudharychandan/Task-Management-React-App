0ec0f2d4a23b28239653c82483f537236e50c396
Chandan Kumar <chandanchaudharyrules@gmail.com>
2018-06-28 00:03:37 +0530

    Create custom theme using createMuiTheme and MuiThemeProvider Wrap App component inside MuiThemeProvider

--- app/components/App/index.js
+++ app/components/App/index.js
@@ -1,21 +1,39 @@
 import React, { Component } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 import Boards from '../../scenes/Boards';
 import Lists from '../../scenes/Lists';
 import Header from '../Header';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#62ebff',
+      main: '#00b8d4',
+      dark: '#0088a3',
+      contrastText: '#000000',
+    },
+    secondary: {
+      light: '#fd558f',
+      main: '#c51162',
+      dark: '#8e0038',
+      contrastText: '#ffffff',
+    },
+  },
+});
+
 class App extends Component {
   render() {
     return (
-      <div>
+      <MuiThemeProvider theme={theme}>
         <Header />
         <Switch>
           <Route exact path='/boards' component={Boards} />
           <Route path='/boards/:id/lists' component={Lists} />
           <Redirect from="/" to="/boards" />
         </Switch>
-      </div>
+      </MuiThemeProvider>
     );
   }
 };
